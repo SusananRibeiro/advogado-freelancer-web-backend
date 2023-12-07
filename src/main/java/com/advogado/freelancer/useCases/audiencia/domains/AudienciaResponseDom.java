280db975294c19a0497d6950a40ed9c6f2b9bbc6
Rafael Pagnan <rafaelmarquespagnan@gmail.com>
2023-12-07 14:54:11 -0300

    adicinado campo cliente e processo no response da audiencia

--- src/main/java/com/advogado/freelancer/useCases/audiencia/domains/AudienciaResponseDom.java
+++ src/main/java/com/advogado/freelancer/useCases/audiencia/domains/AudienciaResponseDom.java
@@ -1,6 +1,8 @@
 package com.advogado.freelancer.useCases.audiencia.domains;
 
 import com.advogado.freelancer.frameWork.utils.Status;
+import com.advogado.freelancer.useCases.clientes.domanis.ClientesResponseDom;
+import com.advogado.freelancer.useCases.processos.domains.ProcessoResponseDom;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -22,6 +24,10 @@ public class AudienciaResponseDom {
     private String local;
 
     private Status status;
+
+    private ClientesResponseDom cliente;
+
+    private ProcessoResponseDom processo;
 
     private Long usuarioId;
     private String nomeCompletoUsuario;
